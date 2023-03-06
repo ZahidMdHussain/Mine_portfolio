d23ac335dad06c4888c8eb9fa0a608e97fef8c26
Zahid <zahimdhussain786@gmail.com>
2023-03-06 17:48:28 +0530

    Developing personal portfolio

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
